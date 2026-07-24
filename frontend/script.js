fb7de1a3a35e5269cbfb215cee42ae0476218d43
Sharanya Naik <sharanyanaik47@gmail.com>
2026-07-24 22:13:57 +0530

    Update frontend script

--- frontend/script.js
+++ frontend/script.js
@@ -11,7 +11,7 @@ async function analyzeWebsite() {
 
     try {
 
-        const response = await fetch("http://localhost:5000/audit", {
+       const response = await fetch("https://page-pulse-kappa.vercel.app/audit", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
